53e2572fe23c498463cf94719e1c8ef3f206a2c0
Gykim28 <gunyoung0113@gmail.com>
2025-01-11 12:28:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+       long x = console.nextLong();
+       long y = console.nextLong();
+       long z = console.nextLong();
+       System.out.println(x+y+z);
+      
+  }
+}
