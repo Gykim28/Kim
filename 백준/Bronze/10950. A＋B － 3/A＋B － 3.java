35881dd387f260d308d484f00002295246bc7a77
Gykim28 <gunyoung0113@gmail.com>
2025-01-17 18:26:52 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17876 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+     Scanner console = new Scanner(System.in);
+     int t = console.nextInt();
+     for (int i=0; i < t; i++){
+         int a = console.nextInt();
+         int b = console.nextInt();
+         System.out.println(a+b);
+     }
+  }
+}
